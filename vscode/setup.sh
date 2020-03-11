03e80b9c2355015e25856ec131916f20a2077409
Ole-Petter Olsen <opolsen@gmail.com>
2020-03-11 16:46:25 +0100

    Add initial setup for VS Code

---
+++ vscode/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER_DIR=$HOME/.config/Code/User
+
+echo "Copying settings to $USER_DIR"
+
+cp keybindings.json $USER_DIR
+cp settings.json $USER_DIR
+
+for extension in $(cat extensions.txt)
+do
+  echo "Installing extension $extension"
+  code --install-extension --force $extension
+done
